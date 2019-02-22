45911d86dce353fb8dcd538aafa7ca8b4af9d2e2
Vaibhav Gupta <vvgonline.net@gmail.com>
2019-02-22 11:18:30 +0530

    created-react-app

---
+++ TODO-react-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+      <div className="App">
+        <header>
+          <h1>
+            Hello React World
+          </h1>
+        </header>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
