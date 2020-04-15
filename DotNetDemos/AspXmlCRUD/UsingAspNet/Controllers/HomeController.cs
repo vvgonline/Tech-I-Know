506ebd98995e91835229b22a3bb5f14e150fc024
Vaibhav V. Gupta <vvgonline.net@gmail.com>
2020-04-16 00:43:05 +0530

    usingAsp.Net in AspXmlCRUD edit remains

--- DotNetDemos/AspXmlCRUD/UsingAspNet/Controllers/HomeController.cs
+++ DotNetDemos/AspXmlCRUD/UsingAspNet/Controllers/HomeController.cs
@@ -14,6 +14,9 @@ namespace UsingAspNet.Controllers
     public class HomeController : Controller
     {
         private const string xmlDocLocation = "~/App_Data/ProjList.xml";
+        // object of model ProjectModels
+        readonly ProjectModels pm = new ProjectModels();
+        #region GET-Methods
         // GET: Home
         public ActionResult Index()
         {
@@ -43,7 +46,37 @@ namespace UsingAspNet.Controllers
         // GET: Home/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            //XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.Load(Server.MapPath(xmlDocLocation));
+            //ProjectModels project = xmlDoc.SelectNodes("/Projects/project")
+            //    .Cast<XmlNode>()
+            //    .Where(node => id.Equals(node["Id"].InnerText)
+            //    .Select(node => new ProjectModels
+            //    {
+            //        ProjId = int.Parse(node["Id"].InnerText),
+            //        ProjName = node["ProjectName"].InnerText,
+            //        Location = node["Location"].InnerText
+            //    }).FirstOrDefault();
+
+            // object of model ProjectModels
+            ProjectModels pm = new ProjectModels();
+            XDocument xDocument = XDocument.Load(Server.MapPath(xmlDocLocation));
+            var items = (from item in xDocument.Descendants("project")
+                         where Convert.ToInt32(item.Element("Id").Value) == id
+                         select new ProjectItems
+                         {
+                             Id = Convert.ToInt32(item.Element("Id").Value),
+                             ProjectName = item.Element("ProjectName").Value,
+                             Location = item.Element("Location").Value
+                         }).SingleOrDefault();
+            if (items != null)
+            {
+                pm.ProjId = items.Id;
+                pm.ProjName = items.ProjectName;
+                pm.Location = items.Location;
+            }
+
+            return View(pm);
         }
 
         // GET: Home/Create
@@ -52,6 +85,67 @@ namespace UsingAspNet.Controllers
             return View();
         }
 
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else
+            {
+                int Id = Convert.ToInt32(id);
+                if (Id > 0)
+                {
+                    Details(Id);
+                    pm.IsEdit = true;
+                    return View(pm);
+                }
+                else
+                {
+                    pm.IsEdit = false;
+                    return View(pm);
+                }
+            }
+            //try
+            //{
+            //    // TODO: Add delete logic here
+            //    XmlDocument XmlDocObj = new XmlDocument();
+            //    XmlDocObj.Load(Server.MapPath(xmlDocLocation));
+            //    XmlNode RootNode = XmlDocObj.SelectSingleNode("Projects");
+            //    XmlNodeList projects = RootNode.ChildNodes;
+
+            //    XmlNode node = projects[0];
+
+            //    foreach (XmlNode n in projects)
+            //    {
+            //        if (int.Parse(n["Id"].InnerText) == id)
+            //        {
+            //            node = n;
+            //        }
+            //    }
+
+            //    node.RemoveAll();
+            //    RootNode.RemoveChild(node);
+
+            //    XmlDocObj.Save(Server.MapPath(xmlDocLocation));
+
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+        #endregion
+
+        #region POST-Methods
         // POST: Home/Create
         [HttpPost]
         public ActionResult Create(ProjectModels project)
@@ -104,32 +198,56 @@ namespace UsingAspNet.Controllers
             }
         }
 
-        // GET: Home/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
         // POST: Home/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(ProjectModels models)//(int id, FormCollection collection)
         {
-            try
-            {
-                // TODO: Add update logic here
+            XDocument xmlDoc = XDocument.Load(Server.MapPath(xmlDocLocation));
+            var items = (from item in xmlDoc.Descendants("Project") select item).ToList();
+            XElement selected = items.Where(p => p.Element("Id").Value == models.ProjId.ToString()).FirstOrDefault();
+            selected.Remove();
+            xmlDoc.Save(Server.MapPath(xmlDocLocation));
+            xmlDoc.Element("Projects").Add(new XElement("Project", new XElement("Id", models.ProjId), new XElement("ProjectName", models.ProjName), new XElement("Location", models.Location)));
+            xmlDoc.Save(Server.MapPath(xmlDocLocation));
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+            xmlDoc.Element("Projects").Add(new XElement("Project", new XElement("Id", models.ProjId), new XElement("ProjectName", models.ProjName), new XElement("Location", models.Location)));
+            xmlDoc.Save(Server.MapPath(xmlDocLocation));
+            return RedirectToAction("Index", "Home");
+            //try
+            //{
+            //    // TODO: Add update logic here
+            //    XmlDocument XmlDocObj = new XmlDocument();
+            //    XmlDocObj.Load(Server.MapPath(xmlDocLocation));
+            //    XmlNode RootNode = XmlDocObj.SelectSingleNode("Projects");
+            //    XmlNodeList projects = RootNode.ChildNodes;
 
-        // GET: Home/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
+            //    XmlNode node = projects[0];
+
+            //    foreach(XmlNode n in projects)
+            //    {
+            //        if(int.Parse(n["Id"].InnerText) == id)
+            //        {
+            //            node = n;
+            //        }
+            //    }
+
+            //    ProjectModels project = new ProjectModels
+            //    {
+            //        ProjName = node.SelectSingleNode("ProjectName").InnerText,
+            //        Location = node.SelectSingleNode("Location").InnerText
+            //    };
+
+            //    node["ProjectName"].InnerText = project.ProjName;
+            //    node["Location"].InnerText = project.Location;
+
+            //    XmlDocObj.Save(Server.MapPath(xmlDocLocation));
+
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
         }
 
         // POST: Home/Delete/5
@@ -139,16 +257,26 @@ namespace UsingAspNet.Controllers
             try
             {
                 // TODO: Add delete logic here
-                if (id > 0)
+                XmlDocument XmlDocObj = new XmlDocument();
+                XmlDocObj.Load(Server.MapPath(xmlDocLocation));
+                XmlNode RootNode = XmlDocObj.SelectSingleNode("Projects");
+                XmlNodeList projects = RootNode.ChildNodes;
+
+                XmlNode node = projects[0];
+
+                foreach(XmlNode n in projects)
                 {
-                    XDocument xmlDoc = XDocument.Load(Server.MapPath(xmlDocLocation));
-                    var items = (from item in xmlDoc.Descendants("Project") select item).ToList();
-                    XElement selected = items.Where(
-                            p => p.Element("Id").Value == id.ToString()
-                        ).FirstOrDefault();
-                    selected.Remove();
-                    xmlDoc.Save(Server.MapPath(xmlDocLocation));
+                    if(int.Parse(n["Id"].InnerText) == id)
+                    {
+                        node = n;
+                    }
                 }
+
+                node.RemoveAll();
+                RootNode.RemoveChild(node);
+
+                XmlDocObj.Save(Server.MapPath(xmlDocLocation));
+
                 return RedirectToAction("Index");
             }
             catch
@@ -156,5 +284,30 @@ namespace UsingAspNet.Controllers
                 return View();
             }
         }
+
+        public ActionResult Update(ProjectModels project)
+        {
+            XmlDocument XmlDocObj = new XmlDocument();
+            XmlDocObj.Load(Server.MapPath(xmlDocLocation));
+            XmlNode RootNode = XmlDocObj.SelectSingleNode("Projects");
+            XmlNodeList projects = RootNode.ChildNodes;
+
+            XmlNode node = projects[0];
+            foreach (XmlNode ns in projects)
+            {
+                if (ns["Id"].InnerText == project.ProjId.ToString())
+                {
+                    node = ns;
+                }
+            }
+
+            node["ProjectName"].InnerText = project.ProjName;
+            node["Location"].InnerText = project.Location;
+
+            XmlDocObj.Save(Server.MapPath(xmlDocLocation));
+
+            return RedirectToAction("Index");
+        }
+        #endregion
     }
 }
