2529b640596808600fca2714e8a14ced1767124b
Vaibhav V. Gupta <vvgonline.net@gmail.com>
2021-02-25 08:33:39 +0530

    changes in youtube api proj

--- DotNetDemos/YouTubeAPI/Program.cs
+++ DotNetDemos/YouTubeAPI/Program.cs
@@ -8,10 +8,10 @@ namespace YouTubeAPI
     {
         static void Main(string[] args)
         {
-            YouTubeService yt = new YouTubeService(new BaseClientService.Initializer() { ApiKey = "YOUR_API_KEY" });
+            YouTubeService yt = new YouTubeService(new BaseClientService.Initializer() { ApiKey = "AIzaSyCinG0shBtUVFt6IVvCDZYVqiefVwPi0Wc" });
 
             var searchListRequest = yt.Search.List("snippet");
-            searchListRequest.ChannelId = "YOUR_CHANNEL_ID";
+            searchListRequest.ChannelId = "UCsFTSQ6exutOIpP-wHl8qQQ";
             var searchListResult = searchListRequest.Execute();
             foreach (var item in searchListResult.Items)
             {
